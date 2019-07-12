6c6d75239d25dd18662591c756872bd35fcfd465
xiaoxiang <36871102@qq.com>
2019-07-12 18:56:05 +0800

    swift lint forward

--- MKVideoManager/MKVideoManager/VideoManager/MKVideoManager.swift
+++ MKVideoManager/MKVideoManager/VideoManager/MKVideoManager.swift
@@ -11,58 +11,49 @@ import UIKit
 import Photos
 
 class MKVideoManager: NSObject {
-    
     static let `default` = MKVideoManager()
-    
     private var movieFilter: GPUImageMovie?
     private var movieWriter: GPUImageMovieWriter?
     private var progressFilter: GPUImageBrightnessFilter?
-    
     private var inputElement: GPUImageUIElement?
-    
-    public var exportUrl: URL = {
+	var exportUrl: URL = {
         return URL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent("story_capture_edit_end_temp.mp4")
     }()
-    
+
     func exportWaterImageVideo(_ watermarkImage: UIImage, _ videoUrl: URL) {
-        let opts: [String: Any] = [AVURLAssetPreferPreciseDurationAndTimingKey: NSNumber.init(booleanLiteral: false)]
-        let asset = AVURLAsset.init(url: videoUrl, options: opts)
+        let opts: [String: Any] = [AVURLAssetPreferPreciseDurationAndTimingKey: NSNumber(booleanLiteral: false)]
+        let asset = AVURLAsset(url: videoUrl, options: opts)
 		let assetVideoTrack = asset.tracks(withMediaType: AVMediaType.video)[0]
         let movieSize = assetVideoTrack.naturalSize
 //        let mp4Url = URL.init(fileURLWithPath: Bundle.main.path(forResource: "test", ofType: "mp4")!)
 //        movieFilter = GPUImageMovie.init(url: mp4Url)
-        movieFilter = GPUImageMovie.init(asset: asset)
+        movieFilter = GPUImageMovie(asset: asset)
         movieFilter?.runBenchmark = false
         //        movieFilter?.playAtActualSpeed = false
         
-        let imageView = UIImageView.init()
+        let imageView = UIImageView()
         imageView.image = watermarkImage.imageMontage()
-        imageView.bounds = CGRect.init(origin: CGPoint.zero, size: movieSize)
+        imageView.bounds = CGRect(origin: CGPoint.zero, size: movieSize)
         imageView.backgroundColor = UIColor.clear
-        imageView.center = CGPoint.init(x: movieSize.width/2, y: movieSize.height/2)
-        let bgView = UIView.init(frame: CGRect.init(x: 0, y: 0, width: movieSize.width, height: movieSize.height))
+        imageView.center = CGPoint(x: movieSize.width / 2, y: movieSize.height / 2)
+        let bgView = UIView(frame: CGRect(x: 0, y: 0, width: movieSize.width, height: movieSize.height))
         bgView.backgroundColor = UIColor.clear
         bgView.addSubview(imageView)
-        inputElement = GPUImageUIElement.init(view: bgView)
-        
+        inputElement = GPUImageUIElement(view: bgView)
         //alphaBlendFilter
         let blendFilter = GPUImageAlphaBlendFilter()
         blendFilter.mix = 1.0
-        
         deleteExistingFile(url: self.exportUrl)
         print("export path : \(exportUrl)")
-        
         //videoSetting
         //        movieWriter = GPUImageMovieWriter.init(movieURL: exportUrl, size: movieSize, fileType: AVFileTypeMPEG4, outputSettings: StoryCaptureConfiguration.videoSetting)
         //        movieWriter?.setHasAudioTrack(true, audioSettings: StoryCaptureConfiguration.audioSetting)
-        movieWriter = GPUImageMovieWriter.init(movieURL: exportUrl, size: movieSize)
+        movieWriter = GPUImageMovieWriter(movieURL: exportUrl, size: movieSize)
         //        movieWriter = GPUImageMovieWriter.init(movieURL: exportUrl, size: movieSize, fileType: AVFileTypeQuickTimeMovie, outputSettings: nil)
         movieWriter?.shouldPassthroughAudio = true
 		movieWriter?.assetWriter.movieFragmentInterval = CMTime.invalid
-        
         movieFilter?.audioEncodingTarget = movieWriter
         movieFilter?.enableSynchronizedEncoding(using: movieWriter)
-        
         progressFilter = GPUImageBrightnessFilter()
         progressFilter?.brightness = 0.0
         //
@@ -71,20 +62,15 @@ class MKVideoManager: NSObject {
         inputElement?.addTarget(blendFilter)
 //        progressFilter?.addTarget(movieWriter)
         blendFilter.addTarget(movieWriter)
-        
         // must set
         progressFilter?.frameProcessingCompletionBlock = { out, time in
             print("time: \(time)")
             self.inputElement?.update()
         }
-        
         movieWriter?.startRecording()
         movieFilter?.startProcessing()
-        
-        
         // success
         movieWriter?.completionBlock = {
-            
             //            self.progressFilter?.removeAllTargets()
             self.movieWriter?.finishRecording()
 //            DispatchQueue.main.sync {
@@ -95,22 +81,19 @@ class MKVideoManager: NSObject {
 //                }
 //            }
         }
-        
+
         // failed
         movieWriter?.failureBlock = { error in
             //            self.progressFilter?.removeTarget(self.movieWriter)
             self.movieWriter?.finishRecording()
         }
     }
-    
-    
     func deleteExistingFile(url: URL) {
         let fileManager = FileManager.default
         do {
             try fileManager.removeItem(at: url)
-        }
-        catch _ as NSError {
-            
+        } catch {
+            print(error.localizedDescription)
         }
     }
 }
@@ -123,4 +106,3 @@ extension UIView {
         return img!
     }
 }
-
--- MKVideoManager/MKVideoManager/outputTool/Models/MKStoryOutput.swift
+++ MKVideoManager/MKVideoManager/outputTool/Models/MKStoryOutput.swift
@@ -16,36 +16,31 @@ public enum MKStoryType {
 }
 
 class TLStoryOutput: NSObject {
-    public      var type:MKStoryType?
+    var type: MKStoryType?
+    var url: URL?
+	var image: UIImage?
+	var audioEnable: Bool = true
+    var movieFile: GPUImageMovie?
+    var movieWriter: GPUImageMovieWriter?
     
-    public      var url:URL?
-    
-    public      var image:UIImage?
-    
-    public      var audioEnable:Bool = true
-    
-    fileprivate var movieFile:GPUImageMovie?
-    
-    fileprivate var movieWriter:GPUImageMovieWriter?
-    
-    public func output(filterNamed:String, container: UIImage, callback:@escaping ((URL?, MKStoryType) -> Void)) {
+    func output(filterNamed: String, container: UIImage, callback:@escaping ((URL?, MKStoryType) -> Void)) {
         if type! == .video {
             self.outputVideo(filterNamed: filterNamed, container: container, audioEnable: audioEnable, callback: callback)
-        }else {
+        } else {
             self.outputImage(filterNamed: filterNamed, container: container, callback: callback)
         }
     }
     
-    public func saveToAlbum(filterNamed:String, container: UIImage, callback:@escaping ((Bool) -> Void)) {
+    func saveToAlbum(filterNamed: String, container: UIImage, callback:@escaping ((Bool) -> Void)) {
         if type! == .video {
             self.outputVideoToAlbum(filterNamed: filterNamed, container: container, audioEnable: audioEnable, callback: callback)
-        }else {
+        } else {
             self.outputImageToAlbum(filterNamed: filterNamed, container: container, callback: callback)
         }
     }
     
-    fileprivate func outputImageToAlbum(filterNamed:String, container: UIImage, callback:@escaping ((Bool) -> Void)) {
-        self.outputImage(filterNamed: filterNamed, container: container) { (u, type) in
+    fileprivate func outputImageToAlbum(filterNamed: String, container: UIImage, callback:@escaping ((Bool) -> Void)) {
+        self.outputImage(filterNamed: filterNamed, container: container) { (u, _) in
             if u == nil {
                 callback(false)
                 return
@@ -53,7 +48,7 @@ class TLStoryOutput: NSObject {
 //            MKProgressHUD.showWatting()
             PHPhotoLibrary.shared().performChanges({
                 PHAssetChangeRequest.creationRequestForAssetFromImage(atFileURL: u!)
-            }, completionHandler: { (x, e) in
+            }, completionHandler: { (_, _) in
                 DispatchQueue.main.async {
 //                    MKProgressHUD.hideWatting()
 //                    MKProgressHUD.show(text: "已保存到相册", delay:1)
@@ -63,8 +58,8 @@ class TLStoryOutput: NSObject {
         }
     }
     
-    fileprivate func outputVideoToAlbum(filterNamed:String, container: UIImage, audioEnable:Bool, callback:@escaping ((Bool) -> Void)) {
-        self.outputVideo(filterNamed: filterNamed, container: container, audioEnable: audioEnable) { (u, type) in
+    fileprivate func outputVideoToAlbum(filterNamed: String, container: UIImage, audioEnable: Bool, callback:@escaping ((Bool) -> Void)) {
+        self.outputVideo(filterNamed: filterNamed, container: container, audioEnable: audioEnable) { (u, _) in
             if u == nil {
                 callback(false)
                 return
@@ -72,7 +67,7 @@ class TLStoryOutput: NSObject {
 //            MKProgressHUD.showWatting()
             PHPhotoLibrary.shared().performChanges({
                 PHAssetChangeRequest.creationRequestForAssetFromVideo(atFileURL: u!)
-            }, completionHandler: { (x, e) in
+            }, completionHandler: { (_, _) in
                 DispatchQueue.main.async {
 //                    MKProgressHUD.hideWatting()
 //                    MKProgressHUD.show(text: "已保存到相册", delay:1)
@@ -82,21 +77,16 @@ class TLStoryOutput: NSObject {
         }
     }
     
-    fileprivate func outputImage(filterNamed:String, container: UIImage, callback:@escaping ((URL?, MKStoryType) -> Void)) {
+    fileprivate func outputImage(filterNamed: String, container: UIImage, callback:@escaping ((URL?, MKStoryType) -> Void)) {
 //        MKProgressHUD.showWatting()
         DispatchQueue.global().async {
-            var cImg:UIImage? = nil
-            
-            if filterNamed != "" {
-                let picture = GPUImagePicture.init(image: self.image!)
-                
-                let filter = GPUImageCustomLookupFilter.init(lookupImageNamed: filterNamed)
-                
+            var cImg: UIImage?
+            if filterNamed.isEmpty == false {
+                let picture = GPUImagePicture(image: self.image!)
+                let filter = GPUImageCustomLookupFilter(lookupImageNamed: filterNamed)
                 picture?.addTarget(filter)
                 picture?.processImage()
-                
                 filter.useNextFrameForImageCapture()
-                
                 guard let img = filter.imageFromCurrentFramebuffer() else {
                     DispatchQueue.main.async(execute: {
 //                        MKProgressHUD.hideWatting()
@@ -106,15 +96,11 @@ class TLStoryOutput: NSObject {
                 }
                 picture?.removeAllTargets()
                 cImg = img
-            }else {
+            } else {
                 cImg = self.image
             }
-            
-            let resultImg = cImg!.imageMontage(img: container,bgColor: UIColor.black,size: MKExportStoryConfiguration.outputPhotoSize).addWatermark(img: MKExportStoryConfiguration.watermarkImage, p: MKExportStoryConfiguration.watermarkPosition)
-            
+            let resultImg = cImg!.imageMontage(img: container, bgColor: UIColor.black, size: MKExportStoryConfiguration.outputPhotoSize).addWatermark(img: MKExportStoryConfiguration.watermarkImage, p: MKExportStoryConfiguration.watermarkPosition)
             let imgData = resultImg.jpegData(compressionQuality: 1)
-
-            
             guard let exportUrl = TLStoryOutput.outputFilePath(type: .photo, isTemp: false) else {
                 DispatchQueue.main.async(execute: {
 //                    MKProgressHUD.hideWatting()
@@ -122,7 +108,6 @@ class TLStoryOutput: NSObject {
                 })
                 return
             }
-            
             DispatchQueue.main.async(execute: {
 //                MKProgressHUD.hideWatting()
                 do {
@@ -135,7 +120,7 @@ class TLStoryOutput: NSObject {
         }
     }
     
-    fileprivate func outputVideo(filterNamed:String, container: UIImage, audioEnable:Bool, callback:@escaping ((URL?, MKStoryType) -> Void)){
+    fileprivate func outputVideo(filterNamed: String, container: UIImage, audioEnable: Bool, callback:@escaping ((URL?, MKStoryType) -> Void)){
         guard let url = url else {
             return
         }
@@ -147,90 +132,83 @@ class TLStoryOutput: NSObject {
         let movieFillFilter = TLGPUImageMovieFillFiter.init()
         movieFillFilter.fillMode = .preserveAspectRatio
         movieFile?.addTarget(movieFillFilter)
-        
+
         guard let exportUrl = TLStoryOutput.outputFilePath(type: .video, isTemp: false) else {
             callback(nil, .video)
             return
         }
         
-        movieWriter = GPUImageMovieWriter.init(movieURL: exportUrl, size: MKExportStoryConfiguration.outputVideoSize)
+        movieWriter = GPUImageMovieWriter(movieURL: exportUrl, size: MKExportStoryConfiguration.outputVideoSize)
         
         if audioEnable {
             movieWriter?.shouldPassthroughAudio = audioEnable
             movieFile?.audioEncodingTarget = movieWriter
         }
         movieFile?.enableSynchronizedEncoding(using: movieWriter)
-        
-        let imgview = UIImageView.init(image: container.addWatermark(img: MKExportStoryConfiguration.watermarkImage, p: MKExportStoryConfiguration.watermarkPosition))
-        
-        let uielement = GPUImageUIElement.init(view: imgview)
-        
+
+        let imgview = UIImageView(image: container.addWatermark(img: MKExportStoryConfiguration.watermarkImage, p: MKExportStoryConfiguration.watermarkPosition))
+
+        let uielement = GPUImageUIElement(view: imgview)
+
         let landBlendFilter = TLGPUImageAlphaBlendFilter.init()
         landBlendFilter.mix = 1
         
-        let progressFilter = filterNamed == "" ? GPUImageFilter.init() : GPUImageCustomLookupFilter.init(lookupImageNamed: filterNamed)
-        
+        let progressFilter = filterNamed.isEmpty ? GPUImageFilter() : GPUImageCustomLookupFilter(lookupImageNamed: filterNamed)
+
         movieFillFilter.addTarget(progressFilter as? GPUImageInput)
         progressFilter.addTarget(landBlendFilter)
         uielement?.addTarget(landBlendFilter)
         landBlendFilter.addTarget(movieWriter!)
-        
+
         progressFilter.frameProcessingCompletionBlock = { output, time in
             uielement?.update(withTimestamp: time)
         }
-        
+
         movieWriter?.startRecording()
         movieFile?.startProcessing()
-        
+
 //        MKProgressHUD.showWatting()
         self.movieWriter?.completionBlock = { [weak self] in
             guard let strongSelf = self else {
                 return
             }
-            
             movieFillFilter.removeAllTargets()
             landBlendFilter.removeAllTargets()
             progressFilter.removeAllTargets()
             uielement?.removeAllTargets()
-            
             strongSelf.movieFile?.removeAllTargets()
             strongSelf.movieWriter?.finishRecording()
             strongSelf.movieFile?.audioEncodingTarget = nil
-            
             DispatchQueue.main.async {
 //                MKProgressHUD.hideWatting()
                 callback(exportUrl, .video)
             }
         }
-        
         self.movieWriter?.failureBlock = { x in
 //            MKProgressHUD.hideWatting()
 //            MKProgressHUD.show(text: "Failure", delay: 0.2)
         }
     }
-    
-    public static func outputFilePath(type:MKStoryType, isTemp:Bool) -> URL? {
+
+	static func outputFilePath(type: MKStoryType, isTemp: Bool) -> URL? {
         do {
             try? FileManager.default.createDirectory(atPath: type == .video ? MKExportStoryConfiguration.videoPath! : MKExportStoryConfiguration.photoPath!, withIntermediateDirectories: true, attributes: nil)
-            
             if type == .video {
                 let fileName = isTemp ? "mov_tmp.mp4" : "mov_out.mp4"
-                let url = URL.init(fileURLWithPath: "\(MKExportStoryConfiguration.videoPath!)/\(fileName)")
+                let url = URL(fileURLWithPath: "\(MKExportStoryConfiguration.videoPath!)/\(fileName)")
                 try? FileManager.default.removeItem(at: url)
                 return url
             }
-            
             if type == .photo {
                 let fileName = isTemp ? "pic_tmp.png" : "pic_out.png"
-                let url = URL.init(fileURLWithPath: "\(MKExportStoryConfiguration.photoPath!)/\(fileName)")
+                let url = URL(fileURLWithPath: "\(MKExportStoryConfiguration.photoPath!)/\(fileName)")
                 try? FileManager.default.removeItem(at: url)
                 return url
             }
         }
         return nil
     }
-    
-    public func reset() {
+	func reset() {
         movieFile?.audioEncodingTarget = nil
         movieFile = nil
         movieWriter = nil
@@ -238,4 +216,3 @@ class TLStoryOutput: NSObject {
         audioEnable = true
     }
 }
-
