c1a78b46f28fecee4c6fb23c282c2f923bb2ea1f
xiaoxiang <36871102@qq.com>
2020-03-02 17:02:07 +0800

    图片浏览器

---
+++ MKVideoManager/MKVideoManager/SubModules/PhotoViewerViewController.swift
@@ -0,0 +1,25 @@
+//
+//  PhotoViewerViewController.swift
+//  MKVideoManager
+//
+//  Created by chenxiaoxiang on 2020/3/2.
+//  Copyright © 2020 xiaoxiang. All rights reserved.
+//
+
+import Foundation
+
+class PhotoViewerController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		self.initAppearance()
+	}
+
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+	}
+
+	func initAppearance() {
+
+	}
+}
--- MKVideoManager/MKVideoManager/ViewController.swift
+++ MKVideoManager/MKVideoManager/ViewController.swift
@@ -10,9 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
     var tableView: UITableView!
-    let modules: [String] = ["文字编辑与UI生成图片", "给视频打水印", "UI交互操作", "取视频封面", "下拉刷新", "Sticker", "adjustTextFont", "滚动动画", "滚动动画2", "滚动动画3", "cell 删除动画", "视频合成、水印、导出、压缩", "Giphy UI", "Tenor gif", "PopTip", "numberLabel", "Slider VC", "WaveForm"]
+    let modules: [String] = ["文字编辑与UI生成图片", "给视频打水印", "UI交互操作", "取视频封面", "下拉刷新", "Sticker", "adjustTextFont", "滚动动画", "滚动动画2", "滚动动画3", "cell 删除动画", "视频合成、水印、导出、压缩", "Giphy UI", "Tenor gif", "PopTip", "numberLabel", "Slider VC", "WaveForm", "图片浏览器"]
 
-    let controllers: [UIViewController.Type] = [MKViewToImageViewController.self, MKVideoEditViewController.self, GestureViewController.self, MKVideoCoverViewController.self, MKRefreshControlViewController.self, MKStickerViewController.self, MKAdjustFontViewController.self, MKScrollTableViewController.self, MKScrollTextViewController.self, MKScrollTextTimerViewController.self, MKDeleteCellTableViewController.self, MKVideoCompositionViewController.self, GiphyUIViewController.self, TenorViewController.self, PopTipViewController.self, ScrollNumberViewController.self, SliderViewController.self, MaskViewController.self]
+    let controllers: [UIViewController.Type] = [MKViewToImageViewController.self, MKVideoEditViewController.self, GestureViewController.self, MKVideoCoverViewController.self, MKRefreshControlViewController.self, MKStickerViewController.self, MKAdjustFontViewController.self, MKScrollTableViewController.self, MKScrollTextViewController.self, MKScrollTextTimerViewController.self, MKDeleteCellTableViewController.self, MKVideoCompositionViewController.self, GiphyUIViewController.self, TenorViewController.self, PopTipViewController.self, ScrollNumberViewController.self, SliderViewController.self, MaskViewController.self, PhotoViewerController.self]
 
 	override func viewDidLoad() {
         super.viewDidLoad()
